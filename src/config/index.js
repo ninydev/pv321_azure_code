37972a205bec19f3d515ad54723849ec5e647d1c
Oleksandr Nykytin <keeper@ninydev.com>
2024-12-02 18:52:56 +0200

    Change to localhost

--- src/config/index.js
+++ src/config/index.js
@@ -8,7 +8,7 @@ dotenv.config();
  *
  */
 export const SERVER_NAME = process.env.SERVER_NAME || "Azure Express";
-export const SERVER_HOST = process.env.SERVER_HOST || "0.0.0.0";
+export const SERVER_HOST = process.env.SERVER_HOST || "localhost";
 export const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 
